86e0dfc1c94028b0a4777ff827f6dcaeb3f312ba
Lorenzo Couto <88043455+aws-coutl@users.noreply.github.com>
2021-09-24 13:01:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
+data "local_file" "foo" {
+    filename = "${path.module}/name_tag.txt"
+}
+
 data "aws_caller_identity" "current" {}
+
 resource "aws_s3_bucket" "main" {
   bucket = "${data.aws_caller_identity.current.account_id}-tf-rds"
   acl    = "private"
 
   tags = {
-    Name        = file("${path.module}/name_tag.txt")
+    Name        = file(data.local_file.foo)
     Environment = "Dev"
   }
 }
